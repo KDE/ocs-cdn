b9de529fc531ebdd21ca1ea2c84026d8137e8fcf
Alexander Schmidt <2f5ed1c5@opayq.com>
2020-08-11 16:44:53 +0200

    updated delete.php

--- public/delete.php
+++ public/delete.php
@@ -23,17 +23,46 @@
 //echo "Deactivated for now";
 //return;
 
+defined('OCS_IMGDELETE_LOGFILE')
+|| define('OCS_IMGDELETE_LOGFILE',
+    (getenv('OCS_IMGDELETE_LOGFILE') ? getenv('OCS_IMGDELETE_LOGFILE') : '../data/logs/delete_' . date("Y-m-d") . '.log'));
+
+defined('OCS_IMGDELETE_LOG_MODE')
+|| define('OCS_IMGDELETE_LOG_MODE', (getenv('OCS_IMGDELETE_LOG_MODE') ? getenv('OCS_IMGDELETE_LOG_MODE') : 'a'));
+
+// Define path to application library
+defined('APPLICATION_LIB')
+|| define('APPLICATION_LIB', realpath(dirname(__DIR__) . '/library'));
+
+// Ensure library/ is on include_path
+set_include_path(implode(PATH_SEPARATOR, array(
+    APPLICATION_LIB,
+    get_include_path(),
+)));
+
+
+require_once APPLICATION_LIB . '/Zend/Log.php';
+require_once APPLICATION_LIB . '/Zend/Log/Writer/Stream.php';
+require_once APPLICATION_LIB . '/Zend/Log/Exception.php';
+$log = new Zend_Log();
+$writer = new Zend_Log_Writer_Stream(OCS_IMGDELETE_LOGFILE, OCS_IMGDELETE_LOG_MODE);
+try {
+    $log->addWriter($writer);
+} catch (Zend_Log_Exception $e) {
+    error_log($e->getMessage());
+}
 
 
 //Constants
 //$CN_APTH = "/mnt/volume-fra1-11/var/www/cdn/pling-cdn";
 $CN_APTH = ".";
 
-if (file_exists('config.php')) {
-    require_once('config.php');
+if (file_exists('../config/config.php')) {
+    require_once('../config/config.php');
 } else {
     header("HTTP/1.0 500 Server Error");
     echo "Error: no config" . PHP_EOL;
+
     return;
 }
 
@@ -45,6 +74,7 @@ $privKey = urldecode($_GET['key']);
 if (!$privKey || $privKey != $configKey) {
     header("HTTP/1.0 500 Server Error");
     echo "Error: param missing" . PHP_EOL;
+
     return;
 }
 
@@ -56,38 +86,41 @@ $imagename = basename($imgurl);
 if (!$imgurl || !$imagename) {
     header("HTTP/1.0 500 Server Error");
     echo "Error, param: path missing" . PHP_EOL;
+
     return;
 }
 if (!$delete_post_name) {
     header("HTTP/1.0 500 Server Error");
     echo "Error, param: post missing" . PHP_EOL;
+
     return;
 }
 
-if($imagename == "std_avatar_80.png") {
+if ($imagename == "std_avatar_80.png") {
     echo "Nothing to do. We do not delete std_avatar_80.png";
+
     return;
 }
 
-echo "Deleting file: ".$imgurl.PHP_EOL;
+echo "Deleting file: " . $imgurl . PHP_EOL;
 
-$fileExists = file_exists($CN_APTH.'/img/' . $imgurl);
+$fileExists = file_exists($CN_APTH . '/img/' . $imgurl);
 
-if($fileExists) {
+if ($fileExists) {
     echo("File exists" . PHP_EOL);
 } else {
     echo("File did not exists" . PHP_EOL);
 }
 
-if($fileExists) {
+if ($fileExists) {
     echo("Rename file..." . PHP_EOL);
-    $last_line = system('mv '.$CN_APTH.'/img/' . $imgurl . ' ' . $CN_APTH.'/img/' . $imgurl . $delete_post_name.' 2>&1');
+    $last_line = system('mv ' . $CN_APTH . '/img/' . $imgurl . ' ' . $CN_APTH . '/img/' . $imgurl . $delete_post_name . ' 2>&1');
     echo $last_line . PHP_EOL;
 
-    $fileExists = file_exists($CN_APTH.'/img/' . $imgurl . $delete_post_name);
+    $fileExists = file_exists($CN_APTH . '/img/' . $imgurl . $delete_post_name);
 
-    if($fileExists) {
-        echo("Rename File done: ".$CN_APTH.'/img/' . $imgurl . $delete_post_name . PHP_EOL);
+    if ($fileExists) {
+        echo("Rename File done: " . $CN_APTH . '/img/' . $imgurl . $delete_post_name . PHP_EOL);
     } else {
         header("HTTP/1.0 500 Server Error");
         echo("Rename File did not work!" . PHP_EOL);
@@ -97,17 +130,17 @@ if($fileExists) {
 echo("Search for cached files:" . PHP_EOL);
 //echo('Command: locate -i "' . $imgurl.'"' . PHP_EOL);
 
-$last_line = exec('locate -i "' . $imgurl.'" 2>&1', $resultArray, $result);
+$last_line = exec('locate -i "' . $imgurl . '" 2>&1', $resultArray, $result);
 
 $numFiles = count($resultArray);
-if($numFiles == 0) {
+if ($numFiles == 0) {
     echo "No cached files found." . PHP_EOL;
 } else {
     echo "Found $numFiles files." . PHP_EOL;
 }
 
 foreach ($resultArray as $value) {
-    if(strpos($value, '/cache/') !== false) {
+    if (strpos($value, '/cache/') !== false) {
         echo "Command: rm $value" . PHP_EOL;
         $last_line = system("rm $value 2>&1");
         echo $last_line . PHP_EOL;
--- public/setup.php
+++ public/setup.php
@@ -1,28 +1,28 @@
 <html>
 <head>
-<title>OSC CDN Setup</title>
+    <title>OSC CDN Setup</title>
 </head>
 <body>
 
 <?php
 
-	if (file_exists('config.php')) {
-		require_once('config.php');
-	}
-	
-	if(!isset($config['privateKey'])) :
-		$configKey = md5(rand()).md5(rand());
+if (file_exists('config.php')) {
+    require_once('config.php');
+}
 
-?>
-	<h1>Private Key</h1>
-	
-	<b>config.php does not exist, or does not define a private key</b><br/>
-	
-	Copy and paste the below text into a config.php file. Once you have created
-	the config file, you will also need to copy and paste the private key into
-	your OSC Webserver configuration file.<br /><br />
-	
-	<textarea style="width: 100%; height: 200px;">
+if (!isset($config['privateKey'])) :
+    $configKey = md5(rand()) . md5(rand());
+
+    ?>
+    <h1>Private Key</h1>
+
+    <b>config.php does not exist, or does not define a private key</b><br/>
+
+    Copy and paste the below text into a config.php file. Once you have created
+    the config file, you will also need to copy and paste the private key into
+    your OSC Webserver configuration file.<br/><br/>
+
+    <textarea style="width: 100%; height: 200px;">
 &lt;?php
 
 	if (!defined('APPLICATION_PATH'))
@@ -33,20 +33,20 @@
 	];
 
 /* End of File */</textarea>
-	
-	<br /><br /><br />
-	<b>OCS Webserver application.ini config value</b>
-	<br />
-	images.media.privateKey = &quot;<?= $configKey ?>&quot;
-	
+
+    <br/><br/><br/>
+    <b>OCS Webserver application.ini config value</b>
+    <br/>
+    images.media.privateKey = &quot;<?= $configKey ?>&quot;
+
 <?php
-	else:
-?>
-	<h1>Information</h1>
-	
-	Everything is up and running! For the private key please refer to the config.php file.
+else:
+    ?>
+    <h1>Information</h1>
+
+    Everything is up and running! For the private key please refer to the config.php file.
 <?php
-	endif;
+endif;
 ?>
 </body>
 </html>
